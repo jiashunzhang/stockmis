03ad48a7da224881a27d79037d09970eb36f68e3
jszhang <jszhang@hebut.edu.cn>
2019-09-19 10:54:15 +0800

    add controller

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -10,7 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
     @RequestMapping("/checkuser")
-    public String CheckUser(String uid, Model model) {
+    public String CheckUser(String uid, String upass, Model model) {
+        String result = "fail";
+        if (uid.equals("aa")&&upass.equals("123")) {
+            result = "success";
+        }
+        model.addAttribute("result", result);
         model.addAttribute("uid", uid);
         return "info";
     }
