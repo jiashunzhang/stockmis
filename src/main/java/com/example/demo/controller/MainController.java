5a0b13fdc39ebb671ce28df2464e242423cdeb8c
jszhang <jiashunzhang@icloud.com>
2019-09-25 22:30:48 +0800

    update jdbc

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class MainController {
+
+    @Autowired
+    JdbcTemplate jdbc;
+
     @RequestMapping("/checkuser")
     public String CheckUser(String uid, String upass, Model model) {
         String result = "fail";
         if (uid.equals("aa")&&upass.equals("123")) {
             result = "success";
         }
+        result = jdbc.queryForObject("select @@version", String.class);
         model.addAttribute("result", result);
         model.addAttribute("uid", uid);
         return "info";
