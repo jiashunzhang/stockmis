7128173cdab8cbf9172854b7085c3261b59e6f38
jszhang <jszhang@office>
2019-11-07 10:48:57 +0800

    add type

--- src/main/java/com/example/demo/controller/StockController.java
+++ src/main/java/com/example/demo/controller/StockController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class StockController {
 
+    @Autowired
+    JdbcTemplate jdbc;
+
     @RequestMapping("/addstock")
     public String addStock(Model model) {
         model.addAttribute("result", "this is add stock");
@@ -18,6 +23,17 @@ public class StockController {
     @RequestMapping("/updatestock")
     public String updateStock(Model model) {
         model.addAttribute("result", "this is update stock");
+        return "info";
+    }
+
+    @RequestMapping("/addstocktype")
+    public String addStockType(String type_name, Model model) {
+        int i = jdbc.update("insert into stock_type values(null, ?) ", type_name);
+        if(i ==1 ) {
+            model.addAttribute("result", "添加成功！");
+        } else {
+            model.addAttribute("result", "添加失败！");            
+        }
         return "info";
     }
     
